a9ab58cdd8a643db0ef9822a6fd3e807ffea1b8a
Arpit-Chaurasia26 <arcdec20@gmail.com>
2026-03-01 20:35:22 +0530

    Initial commit

---
+++ patient-service/src/main/java/com/arpitco/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,29 @@
+package com.arpitco.patient_service.mapper;
+
+import com.arpitco.patient_service.dto.PatientRequestDTO;
+import com.arpitco.patient_service.dto.PatientResponseDTO;
+import com.arpitco.patient_service.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        return patient;
+    }
+}
---
+++ patient-service/src/main/java/com/arpitco/patient_service/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.arpitco.patient_service.repository;
+
+import java.util.UUID;
+import com.arpitco.patient_service.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
+}
