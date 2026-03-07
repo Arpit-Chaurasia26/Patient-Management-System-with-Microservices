f01cbb55aaccb1eea759cce795f3ad194d56f948
Arpit-Chaurasia26 <arcdec20@gmail.com>
2026-03-08 04:19:20 +0530

    Auth service and API Gateway module added

---
+++ auth-service/src/main/java/org/arpitco/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.arpitco.authservice.repository;
+
+import org.arpitco.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
